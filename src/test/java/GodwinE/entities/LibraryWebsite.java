c29d34d01712301c749792ec432624db002f164d
Godwin <godwinmail6@gmail.com>
2023-10-13 13:40:39 +0200

    element can be added to archive

--- src/test/java/GodwinE/entities/LibraryWebsite.java
+++ src/test/java/GodwinE/entities/LibraryWebsite.java
@@ -1,9 +1,11 @@
 package GodwinE.entities;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class LibraryWebsite {
     public static void main(String[] args) {
+
         Book book1 = new Book("ISBN 47895983874I 07466", "Michael Brown", "Romance", "Lovers", "12-12-1994",
                 200);
 
@@ -30,26 +32,82 @@ public class LibraryWebsite {
 
         Book book9 = new Book("ISBN 8209467374684 9984", "Jackie Chan", "Fiction", "Tokyo", "09-08-2014",
                 200);
+
+        //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Magazines<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        List<String> publishingStyle = Arrays.asList("Weekly", "Monthly");
+
+        Magazine mag1 = new Magazine("ISBN 89478489957 9048", "Fashion Fair", "01-09-2003", 39, publishingStyle);
+        Magazine mag2 = new Magazine("ISBN 02745085457 9850", "Vanity Fair", "01-09-2003", 40, publishingStyle);
+        Magazine mag3 = new Magazine("ISBN 20847948595 0948", "Gucci", "07-09-1994", 70, publishingStyle);
+        Magazine mag4 = new Magazine("ISBN 30048754957 0958", "GQ", "19-20-2003", 100, publishingStyle);
+        Magazine mag5 = new Magazine("ISBN 40984085747 9509", "Fashion Italy", "01-02-1970", 88, publishingStyle);
+        Magazine mag6 = new Magazine("ISBN 03974563494 9580", "Mens Fashion", "10-08-2002", 56, publishingStyle);
+        Magazine mag7 = new Magazine("ISBN 38946683099 9489", "Woman", "04-09-1953", 57, publishingStyle);
+
+
+
+
+        List<Book> booksInStore = new ArrayList<>();
+        booksInStore.add(book1);
+        booksInStore.add(book2);
+        booksInStore.add(book3);
+        booksInStore.add(book4);
+        booksInStore.add(book5);
+        booksInStore.add(book6);
+        booksInStore.add(book7);
+        booksInStore.add(book8);
+        booksInStore.add(book9);
+
+
+        Random random = new Random();
+        HashMap<Integer, List<Book>> archive = new HashMap<Integer, List<Book>>();
+        boolean num = false;
+        Scanner input = new Scanner(System.in);
+
+       while(!num) {
+           System.out.println("Type in book title to add to your archive: ");
+           String name = input.nextLine();
+
+           Predicate<Book> isBookTitleTrue = book -> book.getTitle().equals(name);
+
+           List<Book> confirmedBook = booksInStore.stream().filter(isBookTitleTrue).toList();
+           confirmedBook.forEach(book -> System.out.println("This book: " + book + " has been added to your archive" ));
+
+           //remove book using ISBN code
+           System.out.println("Add ISBN to remove Book from Archive: ");
+           int removeThisISBN = Integer.parseInt(input.nextLine());
+
+
+
+       }
+
+
+        input.close();
+
+
+
     }
 
 
-    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Magazines<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 
-    List<String> publishingStyle = Arrays.asList("Weekly", "Monthly");
-
-    Magazine mag1 = new Magazine("ISBN 89478489957 9048", "Fashion Fair", "01-09-2003", 39, publishingStyle);
-    Magazine mag2 = new Magazine("ISBN 02745085457 9850", "Vanity Fair", "01-09-2003", 40, publishingStyle);
-    Magazine mag3 = new Magazine("ISBN 20847948595 0948", "Gucci", "07-09-1994", 70, publishingStyle);
-    Magazine mag4 = new Magazine("ISBN 30048754957 0958", "GQ", "19-20-2003", 100, publishingStyle);
-    Magazine mag5 = new Magazine("ISBN 40984085747 9509", "Fashion Italy", "01-02-1970", 88, publishingStyle);
-    Magazine mag6 = new Magazine("ISBN 03974563494 9580", "Mens Fashion", "10-08-2002", 56, publishingStyle);
-    Magazine mag7 = new Magazine("ISBN 38946683099 9489", "Woman", "04-09-1953", 57, publishingStyle);
 
 
 
-    HashMap<String, Integer> archive = new HashMap<String, Integer>();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
